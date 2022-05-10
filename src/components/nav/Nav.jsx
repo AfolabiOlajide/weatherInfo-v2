8f6efad1631c7fa175547f96001fd220bedbabe8
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-05-10 02:46:08 +0100

    dark mode activated

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Nav.css'
 import Cloud from '../../assets/icons/cloudy.svg'
 
 const Nav = () => {
+    const [lightMode, setLightMode] = useState(true)
+    const changeModeHandler = () => {
+        document.body.classList.toggle('dark-theme-variables');
+        setLightMode(!lightMode)
+    }
+
     return (
     <nav>
         <div className="logo">
             <img src={Cloud} alt="Weather logo" />
             <h2>Weather<span>INFO?</span></h2>
         </div>
-        <div className="toggle">
-            <span className="material-icons-sharp active">light_mode</span>
-            <span className="material-icons-sharp">dark_mode</span>
+        <div onClick={changeModeHandler} className="toggle">
+            <span className={`material-icons-sharp ${lightMode ? 'active' : ''} `}>light_mode</span>
+            <span className={`material-icons-sharp ${lightMode ? '' : 'active'} `}>dark_mode</span>
         </div>
     </nav>
     )
