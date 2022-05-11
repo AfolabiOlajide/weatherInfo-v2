cdd77ff665a92bd406bcac960960681c085ead22
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-05-11 14:41:12 +0100

    solved search button problems

--- src/components/weather/Search.jsx
+++ src/components/weather/Search.jsx
@@ -1,22 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 
 import './Search.css'
 
 const Search = (props) => {
-    const searchWord = useRef()
+    const [ searchText, setSearchText] = useState("");
+
+    const logSesrchTextHandler = (e) => {
+        setSearchText(e.target.value);
+    }
+
 
     const search = (e) => {
         e.preventDefault();
-        console.log(searchWord.current.value);
-        props.onSearch(searchWord.current.value);
+        console.log(searchText);
+        props.onSearch(searchText);
+        setSearchText("")
     }
 
     return (
         <div className="search">
             <form onSubmit={search}>
-                <input ref={searchWord} type="text" className='search-input' placeholder='Search country......'/>
+                <input value={searchText} onChange={logSesrchTextHandler} type="text" className='search-input' placeholder='Search location......'/>
             </form>
-            <div className="search-icon">
+            <div onClick={search} className="search-icon">
                 <span className="material-icons-sharp">search</span>
             </div>
         </div>
